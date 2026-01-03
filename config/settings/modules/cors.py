6d9ce17d1748dd41a9dffb3530d3923ead3241c0
Ozan Metin <ozanmetin99@gmail.com>
2026-01-03 23:16:57 +0300

    Add CORS settings module
    Introduced a new 'cors.py' module for CORS configuration and updated '__init__.py' to include it. This centralizes CORS settings such as allowed origins, headers, methods, and exposed headers for improved maintainability.

---
+++ config/settings/modules/cors.py
@@ -0,0 +1,34 @@
+import environ
+
+env = environ.Env()
+
+# Cors Configuration
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
+CORS_ALLOW_CREDENTIALS = True
+
+# Allowed headers for cross-origin requests
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'origin',
+    'x-csrftoken',
+]
+
+# Allowed HTTP methods
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# Headers that frontend can read from response
+CORS_EXPOSE_HEADERS = [
+    'content-type',
+    'x-csrftoken',
+]
